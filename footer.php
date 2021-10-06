19727c1c40abdae64c9708ca203bb5f3752997a6
Pankajrn12 <91649431+Pankajrn12@users.noreply.github.com>
2021-10-06 03:08:04 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="footer">
+        <!-- footer start -->
+        <div class="container">
+            <!-- container start -->
+            <div class="row">
+                <!-- row start -->
+                <div class="col-md-3 col-sm-6">
+                    <!-- col md start -->
+                    <h4>Page</h4>
+                    <ul>
+                        <li><a href="cart.php">Shopping</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                        <li><a href="shop.php">shop</a></li>
+                        <li><a href="checkout.php">My Account</a></li>
+
+                    </ul>
+                    <hr>
+                    <h4>User Section</h4>
+                    <ul>
+                        <li><a href="checkout.php">Login</a></li>
+                        <li><a href="customer_registration.php">Registration</a></li>
+                    </ul>
+                    <hr class="hidden-md hidden-lg fidden-sm">
+
+                </div>
+                <!-- col md close -->
+                <div class="col-md-3 col-sm-6">
+                    <!-- col-md-3 start -->
+                    <h4>Top Product Ctegories</h4>
+                    <ul>
+                        <li><a href="#">Jacket</a></li>
+                        <li><a href="#">Accessories</a></li>
+                        <li><a href="#">Shoes</a></li>
+                        <li><a href="#">Coat</a></li>
+                        <li><a href="#">T-Shirt</a></li>
+                    </ul>
+                    <hr class="hidden-md hidden-lg">
+
+                </div>
+                <!-- col-md-3 close -->
+                <div class="col-md-3 col-sm-6">
+                    <!-- col-md-3 start -->
+                    <h4>Where to find us</h4>
+                    <p>
+                        <strong>Teehosting.com</strong>
+                        <br>Basapar
+                        <br>Maharajganj
+                        <br>Uttar Pradesh
+                        <br>ravitaprajapati435@gmail.com
+                        <br>+91 7880508014
+                    </p>
+                    <a href="contact.php"> Goto Contact us page</a>
+                    <hr class="hidden-md hidden-lg">
+
+                </div>
+                <!-- col-md-3 close -->
+                <div class="col-md-3 col-sm-6">
+                    <!-- col md start -->
+                    <h4>Get the news</h4>
+                    <p class="text-muted">
+                        Subscribe here for getting news of Maharajganj Uttar Pradesh
+
+                    </p>
+                    <form action="" method="POST">
+                        <div class="input-group">
+                            <input type="text" name="email" class="form-control">
+                            <span class="input-group-btn">
+                                <input type="submit" class="btn btn-default" value="subscribe">
+                            </span>
+
+                        </div>
+
+                    </form>
+                    <hr>
+                    <h4>Stay In Touch</h4>
+                    <p class="social">
+                        <a href="#"><i class="fab fa-facebook-square"></i></a>
+                        <a href="#"><i class="fab fa-whatsapp"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="#"><i class="fab fa-instagram-square"></i></a>
+
+                    </p>
+
+                </div>
+                <!-- col md close -->
+
+            </div>
+            <!-- row cloase -->
+
+        </div>
+        <!-- container close -->
+
+    </div>
+    <!-- footer close -->
+    <div id="copyright">
+        <!-- copyright section start -->
+        <div class="container">
+            <!-- container start -->
+            <div class="col-md-6">
+                <!-- col md start -->
+                <p class="pull-left">
+                    &copy; 2019 Tamplate By: Er. Ravita Prajapati
+
+                </p>
+
+            </div>
+            <!-- col md cloase -->
+            <div class="col-md-6">
+                <div class="pull-right">
+                    Tamplate By: <a href="www.Teehosting.com">Teehosting.com</a>
+  
+                </div>
+
+
+            </div>
+        </div>
+        <!-- container close -->
+
+
+    </div>
+    <!-- copyright section close -->
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,43 @@
+<div class="panel panel-default sidebar-menu">
+    <!-- sidebar menu start -->
+        <div class="panel-heading">
+            <!-- panel heading start -->
+            <h3 class="panel-tittle"> Product Categories</h3>
+    
+        </div>
+        <!-- panel heading close -->
+        <div class="panle-body">
+            <ul class="nav nav-pills nav-stacked category-menu">
+                <li><a href="shop.php">Jacket</a></li>
+                <li><a href="shop.php">Accessories</a></li>
+                <li><a href="shop.php">Shoes</a></li>
+                <li><a href="shop.php">Coats</a></li>
+                <li><a href="shop.php">T-Shirt</a></li>
+
+            </ul>
+
+        </div>
+    
+    </div>
+    <!-- panel sifdebar close -->
+
+
+    <div class="panel panel-default sidebar-menu">
+        <!-- sidebar menu start -->
+            <div class="panel-heading">
+                <!-- panel heading start -->
+                <h3 class="panel-tittle">Categories</h3>
+        
+            </div>
+            <!-- panel heading close -->
+            <div class="panle-body">
+                <ul class="nav nav-pills nav-stacked category-menu">
+                    <li><a href="shop.php">Men</a></li>
+                    <li><a href="shop.php">Women</a></li>
+    
+                </ul>
+    
+            </div>
+        
+        </div>
+        <!-- panel sifdebar close -->
